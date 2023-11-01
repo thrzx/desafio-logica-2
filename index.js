125e8d5d2b6923b759380a697a3a20d08a13be48
thrzx <arthurssilvarp@gmail.com>
2023-11-01 10:45:13 -0300

    Desafio de projeto 2 DIO

---
+++ index.js
@@ -0,0 +1,31 @@
+let v = "200";
+let d = "99";
+getnivel();
+
+function getnivel(v, d) {
+  let saldoVitorias = v - d;
+  let nivel = "";
+  if (saldoVitorias <= 10) {
+    nivel = "Ferro";
+  } else if (saldoVitorias > 10 && saldoVitorias <= 20) {
+    nivel = "Bronze";
+  } else if (saldoVitorias > 20 && saldoVitorias <= 50) {
+    nivel = "Prata";
+  } else if (saldoVitorias > 50 && saldoVitorias <= 80) {
+    nivel = "Ouro";
+  } else if (saldoVitorias > 80 && saldoVitorias <= 90) {
+    nivel = "Diamante";
+  } else if (saldoVitorias > 90 && saldoVitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+  return { saldoVitorias, nivel };
+}
+let resultado = getnivel(v, d);
+console.log(
+  "O Herói tem de saldo de " +
+    resultado.saldoVitorias +
+    " vitórias e está no nível de " +
+    resultado.nivel
+);
